cf18d8be09840b41ded494f1c54422ae89271222
NadyaCodes <nadyacorscadden@gmail.com>
2022-07-12 11:12:51 -0400

    install api packages and install axios

--- api/src/routes/repos.js
+++ api/src/routes/repos.js
@@ -1,12 +1,17 @@
 import { Router, Request, Response } from 'express';
+const axios = require('axios').default;
 
 export const repos = Router();
 
-repos.get('/', async (_: Request, res: Response) => {
+repos.get('/', async (req, res) => {
   res.header('Cache-Control', 'no-store');
 
   res.status(200);
+  const repoArray = []
+
+  axios.get("https://api.github.com/users/silverorange/repos")
+    .then(res => console.log(res))
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  res.json(repoArray);
 });
